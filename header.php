fc6aa42c5269b2f349c30b65b1fc95a68514ca58
Gary McNally <garymcnally@garys-mbp.home>
2019-03-07 22:49:51 +0000

    fixed author and robots rel attributes

--- header.php
+++ header.php
@@ -17,8 +17,8 @@
 <link rel="profile" href="http://gmpg.org/xfn/11">
 <link rel="icon" href="<?php echo get_template_directory_uri(); ?>/images/favicon.png">
 <link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/images/favicon.png">
-<link type="text/plain" rel="robots" href="<?php echo get_template_directory_uri(); ?>/humans.txt" />
-<link type="text/plain" rel="author" href="<?php echo get_template_directory_uri(); ?>/robots.txt" />
+<link type="text/plain" rel="author" href="<?php echo get_template_directory_uri(); ?>/humans.txt" />
+<link type="text/plain" rel="robots" href="<?php echo get_template_directory_uri(); ?>/robots.txt" />
 <?php wp_head(); ?>
 </head>
 
