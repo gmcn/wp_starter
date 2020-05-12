b23bc4d570c40ff4ca6886c22e2c6a127a78c054
Gary McNally <garymcnally@garys-mbp.home>
2020-05-13 00:55:58 +0100

    updated nav

--- header.php
+++ header.php
@@ -26,49 +26,46 @@
 <div id="page" class="site">
 	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'starting-theme' ); ?></a>
 
-	<header id="masthead" class="site-header" role="banner">
-		<div class="site-branding">
-			<?php
-			if ( is_front_page() && is_home() ) : ?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-			<?php else : ?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-			<?php
-			endif;
+	<header>
 
-			$description = get_bloginfo( 'description', 'display' );
-			if ( $description || is_customize_preview() ) : ?>
-				<p class="site-description"><?php echo $description; /* WPCS: xss ok. */ ?></p>
-			<?php
-			endif; ?>
-		</div><!-- .site-branding -->
 
-		<!-- Static navbar -->
-	      <nav class="navbar navbar-default">
-	        <div class="container-fluid">
-	          <div class="navbar-header">
-	            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-	              <span class="sr-only">Toggle navigation</span>
-	              <span class="icon-bar"></span>
-	              <span class="icon-bar"></span>
-	              <span class="icon-bar"></span>
-	            </button>
-	          </div>
-	            <?php wp_nav_menu( array(
-								'theme_location' => 'menu-1',
-								'menu_id' => 'navbar',
-								'container_id' => 'navbar',
-								'container_class' => 'navbar-collapse collapse',
-								'menu_class' => 'navbar-collapse',
-								'items_wrap' => '<ul id="" class="nav navbar-nav navbar-right">%3$s</ul>' ) );
-								?>
-	            <ul class="nav navbar-nav navbar-right">
-	              <li class="active"><a href="./">Default <span class="sr-only">(current)</span></a></li>
-	              <li><a href="../navbar-static-top/">Static top</a></li>
-	              <li><a href="../navbar-fixed-top/">Fixed top</a></li>
-	            </ul>
-	        </div><!--/.container-fluid -->
-	      </nav><!-- #site-navigation -->
-	</header><!-- #masthead -->
+		<nav class="navbar navbar-default navbar-static-top">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="<?php echo esc_url( home_url( '/' ) ); ?>">
+						<?php $description = get_bloginfo( 'description', 'display' ); ?>
+
+						<img src="" alt=" <?php echo $description; /* WPCS: xss ok. */ ?>">
+
+					</a>
+
+
+
+        </div>
+        <div id="navbar" class="navbar-collapse collapse">
+
+					<?php wp_nav_menu( array(
+							'theme_location' => 'menu-1',
+							'menu_id' => 'navbar',
+							'menu_class' => 'navbar-collapse mainnav',
+							'items_wrap' => '<ul id="" class="nav navbar-nav navbar-right">%3$s</ul>' ) );
+							?>
+
+          <ul class="nav navbar-nav navbar-right">
+            <li><a href="../navbar/">Default</a></li>
+            <li class="active"><a href="./">Static top <span class="sr-only">(current)</span></a></li>
+            <li><a href="../navbar-fixed-top/">Fixed top</a></li>
+          </ul>
+        </div><!--/.nav-collapse -->
+      </div>
+    </nav>
+
+	</header><!-- header -->
 
 	<div id="content" class="site-content">
