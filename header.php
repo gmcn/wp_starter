cae2ad4c060d54991b7cf0c718c2de29e4ae8cf7
Gary McNally <garymcnally@gmail.com>
2020-12-10 22:21:56 +0000

    fixed hamburger nav not opening navigation

--- header.php
+++ header.php
@@ -41,7 +41,7 @@
 					'depth'           => 2, // 1 = no dropdowns, 2 = with dropdowns.
 					'container'       => 'div',
 					'container_class' => 'collapse navbar-collapse',
-					'container_id'    => 'bs-example-navbar-collapse-1',
+					'container_id'    => 'navbarCollapse',
 					'menu_class'      => 'navbar-nav mr-auto',
 					'fallback_cb'     => 'WP_Bootstrap_Navwalker::fallback',
 					'walker'          => new WP_Bootstrap_Navwalker(),
