7a1b7cdd5c6f7c78b1b3a819b906809bcc97352e
Gary McNally <garymcnally@gmail.com>
2024-05-08 13:06:57 +0100

    some housekeeping

--- functions.php
+++ functions.php
@@ -88,18 +88,18 @@ add_action( 'after_setup_theme', 'starting_theme_content_width', 0 );
  *
  * @link https://developer.wordpress.org/themes/functionality/sidebars/#registering-a-sidebar
  */
-function starting_theme_widgets_init() {
-	register_sidebar( array(
-		'name'          => esc_html__( 'Sidebar', 'starting-theme' ),
-		'id'            => 'sidebar-1',
-		'description'   => esc_html__( 'Add widgets here.', 'starting-theme' ),
-		'before_widget' => '<section id="%1$s" class="widget %2$s">',
-		'after_widget'  => '</section>',
-		'before_title'  => '<h2 class="widget-title">',
-		'after_title'   => '</h2>',
-	) );
-}
-add_action( 'widgets_init', 'starting_theme_widgets_init' );
+// function starting_theme_widgets_init() {
+// 	register_sidebar( array(
+// 		'name'          => esc_html__( 'Sidebar', 'starting-theme' ),
+// 		'id'            => 'sidebar-1',
+// 		'description'   => esc_html__( 'Add widgets here.', 'starting-theme' ),
+// 		'before_widget' => '<section id="%1$s" class="widget %2$s">',
+// 		'after_widget'  => '</section>',
+// 		'before_title'  => '<h2 class="widget-title">',
+// 		'after_title'   => '</h2>',
+// 	) );
+// }
+// add_action( 'widgets_init', 'starting_theme_widgets_init' );
 
 /**
  * Enqueue scripts and styles.
@@ -114,7 +114,6 @@ function starting_theme_scripts() {
 	wp_enqueue_script( 'swiper-js', get_template_directory_uri() . '/js/swiper-bundle.min.js', array(), '6.2.0', true );
 	wp_enqueue_script( 'functions-js', get_template_directory_uri() . '/js/functions.js', array(), '0.1', true );
 	wp_enqueue_script( 'wow-js', get_template_directory_uri() . '/js/wow.min.js', array(), '0.1', true );
-	wp_enqueue_script( 'matchHeight-js', get_template_directory_uri() . '/js/jquery.matchHeight.js', array(), '0.7.2', true );
 	wp_enqueue_script( 'popper-js', get_template_directory_uri() . '/js/popper.min.js', array(), '1.16.1', true );
 	wp_enqueue_script( 'bootstrap-js', get_template_directory_uri() . '/js/bootstrap.min.js', array(), '4.5.2', true );
 	wp_enqueue_script( 'starting-theme-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
@@ -150,6 +149,22 @@ require get_template_directory() . '/inc/customizer.php';
  */
 require get_template_directory() . '/inc/jetpack.php';
 
+/**
+ * Remove comments to declare WooCommerce
+ */
+// require_once get_template_directory() . '/inc/functions/woocommerce.php';
+
+/**
+ * Custom Post Types
+ */
+require_once get_template_directory() . '/inc/functions/cpt.php';
+
+/**
+ * acf support
+ */
+require_once get_template_directory() . '/inc/functions/acf.php';
+
+// bootstrap 4 support
 
 //add data-toggle class and dropdown-toggle to parent anchor link
 function addanchorlink_class($menu) {
@@ -167,183 +182,13 @@ function new_submenu_class($menu) {
 
 add_filter('wp_nav_menu','new_submenu_class');
 
-// Make the search to index custom
-/**
- * Extend WordPress search to include custom fields
- * http://adambalee.com
- *
- * Join posts and postmeta tables
- * http://codex.wordpress.org/Plugin_API/Filter_Reference/posts_join
- */
-function cf_search_join( $join ) {
-    global $wpdb;
-    if ( is_search() ) {
-        $join .=' LEFT JOIN '.$wpdb->postmeta. ' ON '. $wpdb->posts . '.ID = ' . $wpdb->postmeta . '.post_id ';
-    }
-    return $join;
-}
-add_filter('posts_join', 'cf_search_join' );
-
-/**
- * Modify the search query with posts_where
- * http://codex.wordpress.org/Plugin_API/Filter_Reference/posts_where
- */
-function cf_search_where( $where ) {
-    global $pagenow, $wpdb;
-    if ( is_search() ) {
-        $where = preg_replace(
-            "/\(\s*".$wpdb->posts.".post_title\s+LIKE\s*(\'[^\']+\')\s*\)/",
-            "(".$wpdb->posts.".post_title LIKE $1) OR (".$wpdb->postmeta.".meta_value LIKE $1)", $where );
-    }
-    return $where;
-}
-add_filter( 'posts_where', 'cf_search_where' );
-
-/**
- * Prevent duplicates
- * http://codex.wordpress.org/Plugin_API/Filter_Reference/posts_distinct
- */
-function cf_search_distinct( $where ) {
-    global $wpdb;
-    if ( is_search() ) {
-        return "DISTINCT";
-    }
-    return $where;
-}
-add_filter( 'posts_distinct', 'cf_search_distinct' );
-
-/**
- * Code to add the custom login css file to the theme
- * - file is "/login/custom-login-styles.css"
- */
-function my_custom_login() {
-echo '<link rel="stylesheet" type="text/css" href="' . get_bloginfo('stylesheet_directory') . '/login/custom-login-styles.css" />';
-}
-add_action('login_head', 'my_custom_login');
-
-// Register Custom Post Type
-// function projects_post_type() {
-//
-// 	$labels = array(
-// 		'name'                  => _x( 'Projects', 'Post Type General Name', 'text_domain' ),
-// 		'singular_name'         => _x( 'Project', 'Post Type Singular Name', 'text_domain' ),
-// 		'menu_name'             => __( 'Projects', 'text_domain' ),
-// 		'name_admin_bar'        => __( 'Project', 'text_domain' ),
-// 		'archives'              => __( 'Item Archives', 'text_domain' ),
-// 		'attributes'            => __( 'Item Attributes', 'text_domain' ),
-// 		'parent_item_colon'     => __( 'Parent Item:', 'text_domain' ),
-// 		'all_items'             => __( 'All Items', 'text_domain' ),
-// 		'add_new_item'          => __( 'Add New Item', 'text_domain' ),
-// 		'add_new'               => __( 'Add New', 'text_domain' ),
-// 		'new_item'              => __( 'New Item', 'text_domain' ),
-// 		'edit_item'             => __( 'Edit Item', 'text_domain' ),
-// 		'update_item'           => __( 'Update Item', 'text_domain' ),
-// 		'view_item'             => __( 'View Item', 'text_domain' ),
-// 		'view_items'            => __( 'View Items', 'text_domain' ),
-// 		'search_items'          => __( 'Search Item', 'text_domain' ),
-// 		'not_found'             => __( 'Not found', 'text_domain' ),
-// 		'not_found_in_trash'    => __( 'Not found in Trash', 'text_domain' ),
-// 		'featured_image'        => __( 'Featured Image', 'text_domain' ),
-// 		'set_featured_image'    => __( 'Set featured image', 'text_domain' ),
-// 		'remove_featured_image' => __( 'Remove featured image', 'text_domain' ),
-// 		'use_featured_image'    => __( 'Use as featured image', 'text_domain' ),
-// 		'insert_into_item'      => __( 'Insert into item', 'text_domain' ),
-// 		'uploaded_to_this_item' => __( 'Uploaded to this item', 'text_domain' ),
-// 		'items_list'            => __( 'Items list', 'text_domain' ),
-// 		'items_list_navigation' => __( 'Items list navigation', 'text_domain' ),
-// 		'filter_items_list'     => __( 'Filter items list', 'text_domain' ),
-// 	);
-// 	$args = array(
-// 		'label'                 => __( 'Project', 'text_domain' ),
-// 		'description'           => __( 'Projects information page.', 'text_domain' ),
-// 		'labels'                => $labels,
-// 		'supports'              => array( 'title', 'editor', 'author', 'thumbnail', 'excerpt', 'revisions', 'featured' ),
-// 		'taxonomies'            => array( 'category' ),
-// 		'hierarchical'          => false,
-// 		'public'                => true,
-// 		'show_ui'               => true,
-// 		'show_in_menu'          => true,
-// 		'menu_position'         => 5,
-// 		'menu_icon'             => 'dashicons-clipboard',
-// 		'show_in_admin_bar'     => true,
-// 		'show_in_nav_menus'     => true,
-// 		'can_export'            => true,
-// 		'has_archive'           => true,
-// 		'exclude_from_search'   => false,
-// 		'publicly_queryable'    => true,
-// 		'capability_type'       => 'page',
-// 	);
-// 	register_post_type( 'projects', $args );
-//
-// }
-// add_action( 'init', 'projects_post_type', 0 );
-//
-// add_action( 'init', 'projects_taxonomies', 0 );
-//
-// function projects_taxonomies() {
-//
-//     // Product Type Taxonomy
-//     $product_cat_labels = array(
-//         'name'          => 'Project Category',
-//         'singular_name' => 'Project Category',
-//         'menu_name'     => 'Project Categories'
-//     );
-//
-//     $product_cat_args = array(
-//         'labels'                     => $product_cat_labels,
-//         'hierarchical'               => true,
-//         'public'                     => true,
-//         'show_ui'                    => true,
-//         'show_admin_column'          => true,
-//         'show_in_nav_menus'          => true,
-//         'show_tagcloud'              => true,
-//         'rewrite'                    => array('pages' => true)
-//     );
-//     register_taxonomy( 'projects_category', array( 'projects' ), $product_cat_args );
-//
-// 		// Product Type Taxonomy
-//     $product_type_labels = array(
-//         'name'          => 'Project Type',
-//         'singular_name' => 'Project Type',
-//         'menu_name'     => 'Project Types'
-//     );
-//
-//     $product_type_args = array(
-//         'labels'                     => $product_type_labels,
-//         'hierarchical'               => true,
-//         'public'                     => true,
-//         'show_ui'                    => true,
-//         'show_admin_column'          => true,
-//         'show_in_nav_menus'          => true,
-//         'show_tagcloud'              => true,
-//         'rewrite'                    => array('pages' => true)
-//     );
-//     register_taxonomy( 'projects_type', array( 'projects' ), $product_type_args );
-//
-// }
-
-// bootstrap 4 support
-
 /**
  * Register Custom Navigation Walker
  */
 function register_navwalker(){
-	require_once get_template_directory() . '/functions/class-wp-bootstrap-navwalker.php';
+	require_once get_template_directory() . '/inc/functions/class-wp-bootstrap-navwalker.php';
 }
 add_action( 'after_setup_theme', 'register_navwalker' );
-
-// SVG support for remove_featured_image
-function cc_mime_types($mimes) {
- $mimes['svg'] = 'image/svg+xml';
- return $mimes;
-}
-add_filter('upload_mimes', 'cc_mime_types');
-
-// Move Yoast to bottom
-function yoasttobottom() {
-	return 'low';
-}
-add_filter( 'wpseo_metabox_prio', 'yoasttobottom');
 
 //* Enqueue script to activate WOW.js
 add_action('wp_enqueue_scripts', 'sk_wow_init_in_footer');
--- inc/extras.php
+++ inc/extras.php
@@ -37,3 +37,72 @@ function starting_theme_pingback_header() {
 	}
 }
 add_action( 'wp_head', 'starting_theme_pingback_header' );
+
+
+// Make the search to index custom
+/**
+ * Extend WordPress search to include custom fields
+ * http://adambalee.com
+ *
+ * Join posts and postmeta tables
+ * http://codex.wordpress.org/Plugin_API/Filter_Reference/posts_join
+ */
+function cf_search_join( $join ) {
+    global $wpdb;
+    if ( is_search() ) {
+        $join .=' LEFT JOIN '.$wpdb->postmeta. ' ON '. $wpdb->posts . '.ID = ' . $wpdb->postmeta . '.post_id ';
+    }
+    return $join;
+}
+add_filter('posts_join', 'cf_search_join' );
+
+/**
+ * Modify the search query with posts_where
+ * http://codex.wordpress.org/Plugin_API/Filter_Reference/posts_where
+ */
+function cf_search_where( $where ) {
+    global $pagenow, $wpdb;
+    if ( is_search() ) {
+        $where = preg_replace(
+            "/\(\s*".$wpdb->posts.".post_title\s+LIKE\s*(\'[^\']+\')\s*\)/",
+            "(".$wpdb->posts.".post_title LIKE $1) OR (".$wpdb->postmeta.".meta_value LIKE $1)", $where );
+    }
+    return $where;
+}
+add_filter( 'posts_where', 'cf_search_where' );
+
+/**
+ * Prevent duplicates
+ * http://codex.wordpress.org/Plugin_API/Filter_Reference/posts_distinct
+ */
+function cf_search_distinct( $where ) {
+    global $wpdb;
+    if ( is_search() ) {
+        return "DISTINCT";
+    }
+    return $where;
+}
+add_filter( 'posts_distinct', 'cf_search_distinct' );
+
+/**
+ * Code to add the custom login css file to the theme
+ * - file is "/login/custom-login-styles.css"
+ */
+function my_custom_login() {
+echo '<link rel="stylesheet" type="text/css" href="' . get_bloginfo('stylesheet_directory') . '/login/custom-login-styles.css" />';
+}
+add_action('login_head', 'my_custom_login');
+
+
+// SVG support for remove_featured_image
+function cc_mime_types($mimes) {
+ $mimes['svg'] = 'image/svg+xml';
+ return $mimes;
+}
+add_filter('upload_mimes', 'cc_mime_types');
+
+// Move Yoast to bottom
+function yoasttobottom() {
+	return 'low';
+}
+add_filter( 'wpseo_metabox_prio', 'yoasttobottom');
